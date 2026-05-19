68db98852a1273a78e45d96cebeef5767204c9d9
Josh Hall <jrbhall@outlook.com>
2026-05-19 11:05:14 +0100

    Add @/ path alias to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,16 @@
+import path from 'path';
+
 import dotenv from 'dotenv';
 import { defineConfig } from 'vitest/config';
 
 dotenv.config({ path: '.env.test' });
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     include: ['src/**/*.test.ts'],
     environment: 'node',
